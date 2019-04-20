9134c39dcf0353e24300860e44bcdad0679cc42e
Neill Bolton <toovs@tutamail.com>
2019-04-19 20:20:52 -0700

    caps: subs-exempt

--- src/main.rs
+++ src/main.rs
@@ -285,10 +285,10 @@ fn register_handler(client: IrcClient, reactor: &mut IrcReactor, con: Arc<r2d2::
                                 if len >= trigger {
                                     let num = msg.chars().fold(0.0, |acc, c| if c.is_uppercase() { acc + 1.0 } else { acc });
                                     let ratio = num / len;
-                                    if ratio > (limit / 100.0) {
+                                    if ratio >= (limit / 100.0) {
                                         if !subscriber || subscriber && subs == "true" {
                                             let _ = client.send_privmsg(chan, format!("/timeout {} 1", nick));
-                                            if display == "true" { let _ = client.send_privmsg(chan, format!("@{} you've been timed out for posting colors", nick)); }
+                                            if display == "true" { let _ = client.send_privmsg(chan, format!("@{} you've been timed out for posting too many caps", nick)); }
                                         }
                                     }
                                 }
