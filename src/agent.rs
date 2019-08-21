f9d1f5f37b9508e3c1938269aea37a7750344106
Neill Bolton <toovs@tutamail.com>
2019-08-21 16:30:13 -0700

    agent update

--- src/agent.rs
+++ src/agent.rs
@@ -1,4 +1,5 @@
 #[cfg(windows)] use winapi::um::winuser::{INPUT_u, INPUT, INPUT_KEYBOARD, KEYBDINPUT, SendInput};
+use std::{thread,time};
 use config;
 use redis::{self,Commands};
 use http::header::{self,HeaderValue};
@@ -33,110 +34,114 @@ fn main() {
     let r2 = settings.get_str("password");
     let r3 = settings.get_str("base_url");
     if let (Ok(channel), Ok(password), Ok(base_url)) = (r1, r2, r3) {
-        let mut headers = header::HeaderMap::new();
-        headers.insert("Content-Type", HeaderValue::from_str("application/x-www-form-urlencoded").unwrap());
-        let client = Client::builder().default_headers(headers).build().unwrap();
-        let rsp = client.post(&format!("{}/api/agent", base_url)).body(format!("channel={}&password={}", &channel, &password).as_bytes().to_owned()).send();
-        match rsp {
-            Err(e) => { println!("response error: {}", &e.to_string()); }
-            Ok(mut rsp) => {
-                let text = rsp.text().unwrap();
-                let json: Result<AgentRsp,_> = serde_json::from_str(&text);
-                match json {
-                    Err(e) => {
-                        println!("response error: {}", &e.to_string());
-                    }
-                    Ok(json) => {
-                        if VERSION >= json.version {
-                            if json.success {
-                                if let (Some(action), Some(args)) = (json.action, json.args) {
-                                    println!("received action: {}", action);
-                                    match action.as_ref() {
-                                        "INPUT" => {
-                                            for arg in args.clone() {
-                                                let mut input_u: INPUT_u = unsafe { std::mem::zeroed() };
-                                                unsafe {
-                                                    *input_u.ki_mut() = KEYBDINPUT {
-                                                        wVk: 0,
-                                                        dwFlags: 0,
-                                                        dwExtraInfo: 0,
-                                                        wScan: 0,
-                                                        time: 0
+        loop {
+            let mut headers = header::HeaderMap::new();
+            headers.insert("Content-Type", HeaderValue::from_str("application/x-www-form-urlencoded").unwrap());
+            let client = Client::builder().default_headers(headers).build().unwrap();
+            let rsp = client.post(&format!("{}/api/agent", base_url)).body(format!("channel={}&password={}", &channel, &password).as_bytes().to_owned()).send();
+            match rsp {
+                Err(e) => { println!("response error: {}", &e.to_string()); }
+                Ok(mut rsp) => {
+                    let text = rsp.text().unwrap();
+                    let json: Result<AgentRsp,_> = serde_json::from_str(&text);
+                    match json {
+                        Err(e) => {
+                            println!("response error: {}", &e.to_string());
+                        }
+                        Ok(json) => {
+                            if VERSION >= json.version {
+                                if json.success {
+                                    if let (Some(action), Some(args)) = (json.action, json.args) {
+                                        println!("received action: {}", action);
+                                        match action.as_ref() {
+                                            "INPUT" => {
+                                                for arg in args.clone() {
+                                                    let mut input_u: INPUT_u = unsafe { std::mem::zeroed() };
+                                                    unsafe {
+                                                        *input_u.ki_mut() = KEYBDINPUT {
+                                                            wVk: 0,
+                                                            dwFlags: 0,
+                                                            dwExtraInfo: 0,
+                                                            wScan: 0,
+                                                            time: 0
+                                                        }
                                                     }
+
+                                                    let mut input = INPUT {
+                                                        type_: INPUT_KEYBOARD,
+                                                        u: input_u
+                                                    };
+                                                    let ipsize = std::mem::size_of::<INPUT>() as i32;
+
+                                                    unsafe {
+                                                        SendInput(1, &mut input, ipsize);
+                                                    };
                                                 }
 
-                                                let mut input = INPUT {
-                                                    type_: INPUT_KEYBOARD,
-                                                    u: input_u
-                                                };
-                                                let ipsize = std::mem::size_of::<INPUT>() as i32;
-
-                                                unsafe {
-                                                    SendInput(1, &mut input, ipsize);
-                                                };
-                                            }
-
-                                            for arg in args.clone() {
-                                                let mut input_u: INPUT_u = unsafe { std::mem::zeroed() };
-                                                unsafe {
-                                                    *input_u.ki_mut() = KEYBDINPUT {
-                                                        wVk: KEYUP,
-                                                        dwFlags: 0,
-                                                        dwExtraInfo: 0,
-                                                        wScan: 0,
-                                                        time: 0
+                                                for arg in args.clone() {
+                                                    let mut input_u: INPUT_u = unsafe { std::mem::zeroed() };
+                                                    unsafe {
+                                                        *input_u.ki_mut() = KEYBDINPUT {
+                                                            wVk: KEYUP,
+                                                            dwFlags: 0,
+                                                            dwExtraInfo: 0,
+                                                            wScan: 0,
+                                                            time: 0
+                                                        }
                                                     }
+
+                                                    let mut input = INPUT {
+                                                        type_: INPUT_KEYBOARD,
+                                                        u: input_u
+                                                    };
+                                                    let ipsize = std::mem::size_of::<INPUT>() as i32;
+
+                                                    unsafe {
+                                                        SendInput(1, &mut input, ipsize);
+                                                    };
                                                 }
-
-                                                let mut input = INPUT {
-                                                    type_: INPUT_KEYBOARD,
-                                                    u: input_u
-                                                };
-                                                let ipsize = std::mem::size_of::<INPUT>() as i32;
-
-                                                unsafe {
-                                                    SendInput(1, &mut input, ipsize);
-                                                };
                                             }
+                                            _ => {}
                                         }
-                                        _ => {}
+                                    }
+                                } else {
+                                    if let Some(msg) = json.error_message {
+                                        println!("response error: {}", msg);
                                     }
                                 }
                             } else {
-                                if let Some(msg) = json.error_message {
-                                    println!("response error: {}", msg);
-                                }
-                            }
-                        } else {
-                            let r1 = settings.get_str("github_owner");
-                            let r2 = settings.get_str("github_name");
-                            if let (Ok(owner), Ok(name)) = (r1, r2) {
-                                let res = self_update::backends::github::Update::configure()
-                                    .repo_owner(&owner)
-                                    .repo_name(&name)
-                                    .bin_name("babblebot")
-                                    .show_download_progress(true)
-                                    .current_version(cargo_crate_version!())
-                                    .build();
-                                match res {
-                                    Err(e) => println!("update error: {}", &e.to_string()),
-                                    Ok(update) => {
-                                        match update.update() {
-                                            Err(e) => println!("update error: {}", &e.to_string()),
-                                            Ok(status) => {
-                                                println!("updated to version: {}", status.version());
+                                let r1 = settings.get_str("github_owner");
+                                let r2 = settings.get_str("github_name");
+                                if let (Ok(owner), Ok(name)) = (r1, r2) {
+                                    let res = self_update::backends::github::Update::configure()
+                                        .repo_owner(&owner)
+                                        .repo_name(&name)
+                                        .bin_name("babblebot")
+                                        .show_download_progress(true)
+                                        .current_version(cargo_crate_version!())
+                                        .build();
+                                    match res {
+                                        Err(e) => println!("update error: {}", &e.to_string()),
+                                        Ok(update) => {
+                                            match update.update() {
+                                                Err(e) => println!("update error: {}", &e.to_string()),
+                                                Ok(status) => {
+                                                    println!("updated to version: {}", status.version());
+                                                }
                                             }
                                         }
                                     }
-                                }
 
-                            } else {
-                                println!("version error: your client is out of date");
+                                } else {
+                                    println!("version error: your client is out of date");
+                                }
                             }
                         }
                     }
                 }
             }
+
+        thread::sleep(time::Duration::from_secs(10));
         }
     }
 }
